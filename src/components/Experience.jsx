742a75d17135343908e0d9775a3e2b75e9909029
Clayton Lewis <lewisclayton89@gmail.com>
2022-01-31 23:28:26 -0500

    experience layout added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,6 +11,25 @@ function Experience() {
   return (
     <div className="expo-holder">
       <h1>Experience</h1>
+   <div className="expo-flex">
+
+   <div className="work">
+      <h2>Place of Business</h2>
+      <div className="expo-flex-1">
+
+      </div>
+    </div>
+
+    <div className="skills">
+    <h2>Skills</h2>
+    <div className="expo-flex-2">
+
+    </div>
+    </div>
+
+
+   </div>
+   <h1>Resume</h1>
    </div>
   );
 }
