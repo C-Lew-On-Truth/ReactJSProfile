b8717ba9ec4016e770d324c920d51371fe69df17
Clayton Lewis <lewisclayton89@gmail.com>
2022-02-22 13:03:13 -0500

    rwd

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class App extends React.Component {
        }
 
         render() {
+                console.log(window.innerWidth)
                 return (
                         <div>
                                <div className="main">
