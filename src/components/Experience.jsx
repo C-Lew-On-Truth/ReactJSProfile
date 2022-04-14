d3cd58f979e92a1ee44f67f01ff52e00480522a8
Clayton Lewis <lewisclayton89@gmail.com>
2022-04-14 15:36:53 -0400

    rwd progress on experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,73 +1,78 @@
-
-import React from 'react';
-import '../style/experience.css';
-import SeenThis from "../media/SeenThis.jpeg"
-import NY1 from "../media/ny1.jpg"
-import Meredith from "../media/meredith.jpg"
-import Adobe from "../media/adobe.jpg"
+import React from "react";
+import "../style/experience.css";
+import SeenThis from "../media/SeenThis.jpeg";
+import NY1 from "../media/ny1.jpg";
+import Meredith from "../media/meredith.jpg";
+import Adobe from "../media/adobe.jpg";
 
 class Experience extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.fadeExpo = React.createRef()
-    this.showExpo = this.showExpo.bind(this)
+    this.fadeExpo = React.createRef();
+    this.showExpo = this.showExpo.bind(this);
   }
-/*
+  /*
   componentDidMount() {
     this.showExpo()
   }
 */
   showExpo() {
-    let expo = this.fadeExpo.current
+    let expo = this.fadeExpo.current;
     setTimeout(() => {
-        expo.classList.add("fade-expo")
-    }, 1000)
- }
+      expo.classList.add("fade-expo");
+    }, 1000);
+  }
 
-render() {
-  return (
-    <div className="expo-holder" ref={this.fadeExpo}>
-   <div className="expo-flex">
+  render() {
+    return (
+      <div className="expo-holder">
 
-   <div className="work">
-      <h2 className="business-head">Companies</h2>
-      <div className="expo-flex-1">
+        <div className="expo-flex">
+          <div className="work">
 
- 
-     <div className="companies">
-          <img className="icons" src={SeenThis}/>
-          <h3>SeenThis</h3>
+          <h2 className="business-head">Companies</h2>
+
+            <div className="expo-flex-1">
+            
+              <div className="companies">
+                <img className="icons" src={SeenThis} />
+                <h3>SeenThis</h3>
+                <h4>Technical Producer</h4>
+              </div>
+
+              <div className="companies">
+                <img className="icons" src={NY1} />
+                <h3>Spectrum News NY1</h3>
+                <h4>Media Ingest Operator</h4>
+              </div>
+
+              <div className="companies">
+                <img className="icons" src={Meredith} />
+                <h3>Meredith</h3>
+                <h4>Video Ingest Technician</h4>
+              </div>
+
+              <div className="companies">
+                <img className="icons" src={Adobe} />
+                <h3>Adobe Stock Media</h3>
+                <h4>Media Operations Coordinator</h4>
+              </div>
+      
+
+            </div>
+            <h2 className="resume">Resume</h2>
+          </div>
+
+          <div className="skills">
+            <h2 className="skills-head">Skills</h2>
+            <div className="expo-flex-2"></div>
+          </div>
         </div>
-        <div className="companies">
-          <img className="icons" src={NY1}/>
-          <h3>New York One</h3>
-        </div>
-        <div className="companies">
-          <img className="icons" src={Meredith}/>
-          <h3>Meredith</h3>
-        </div>
-        <div className="companies">
-          <img className="icons" src={Adobe}/>
-          <h3>Adobe</h3>
-        </div>
-     
+        
       </div>
-    </div>
-
-    <div className="skills">
-    <h2 className="skills-head">Skills</h2>
-    <div className="expo-flex-2">
-       
-    </div>
-    </div>
-
-
-   </div>
-   <h1 className="resume">Resume</h1>
-   </div>
-  );
-}
+    );
+  }
 }
 
-export default Experience
+export default Experience;
